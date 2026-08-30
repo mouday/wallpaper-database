cd6a6614bd878e283dedcd6bc58bb7513481ef09
mouday <pengshiyuyx@gmail.com>
2026-08-30 15:23:54 +0800

    Enhance headers in get_bing_image function
    Added Accept and Accept-Language headers to the request.
    
    Signed-off-by: mouday <pengshiyuyx@gmail.com>

--- bing_image.py
+++ bing_image.py
@@ -16,7 +16,9 @@ def get_bing_image():
     url = 'https://cn.bing.com'
 
     headers = {
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36'
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
     }
 
     res = requests.get(url, headers=headers)
